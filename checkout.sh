919d845707e96762386bae7bb6c97bdfdd9cf812
Andrey Bodoev <andrey.bodoev@zenika.com>
2020-10-21 18:18:38 +0800

    feat: added 100

---
+++ checkout.sh
@@ -0,0 +1,15 @@
+VERSION=$1
+touch filename-$VERSION.md
+
+BRANCH_NAME=new/$VERSION
+
+BRANCH_EXISTS="$(git ls-remote --exit-code --heads origin "$BRANCH_NAME" | wc -l)"
+
+if [ "$BRANCH_EXISTS" -eq "1" ]; then
+    git checkout "$BRANCH_NAME"
+else
+    git checkout -b "$BRANCH_NAME"
+fi;
+
+git add .
+git commit -am "feat: added $VERSION"
