d6a11967d043f6f8adb5055bd81098c87f10c1e6
Andrey Bodoev <andrey.bodoev@zenika.com>
2020-10-21 18:33:47 +0800

    feat: added 160

--- checkout.sh
+++ checkout.sh
@@ -4,10 +4,13 @@ touch filename-$VERSION.md
 BRANCH_NAME=new/120
 
 BRANCH_EXISTS="$(git ls-remote --exit-code --heads origin "$BRANCH_NAME" | wc -l)"
+echo $BRANCH_EXISTS
 
 if [ "$BRANCH_EXISTS" -eq "1" ]; then
+    echo "1"
     git checkout "$BRANCH_NAME"
 else
+    echo "2"
     git checkout -b "$BRANCH_NAME"
 fi;
 
